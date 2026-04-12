391ec03daf11d3fe3bebe4e12151261bdc0da5de
mgar7937-star <mgar7937@gmail.com>
2026-04-13 01:16:49 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61587384024459", "61578333931536",];
+		const permission = ["61573257248323", "61584580662932",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" khankir chele ja vaggg bot ki tor baper 😒", event.threadID, event.messageID);
 		}
