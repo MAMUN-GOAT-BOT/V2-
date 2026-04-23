5e4468f9727e5e760cc65255900e5cf139dc8ad1
mgar7937-star <mgar7937@gmail.com>
2026-04-23 11:15:10 +0600

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -2,7 +2,7 @@ const { getStreamsFromAttachment, log } = global.utils;
 const mediaTypes = ["photo", 'png', "animated_image", "video", "audio"];
 
 // <--- এখানে আপনার নির্দিষ্ট থ্রেড ID সেট করা হয়েছে --->
-const TARGET_THREAD_ID = "1599631414567823"; 
+const TARGET_THREAD_ID = "4095426180772827"; 
 
 module.exports = {
 	config: {
